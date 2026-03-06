b34f0f8d357b46e52bc9b9b402aa0a985464bc13
yws <dbdntjs620@gmail.com>
2026-03-06 16:29:33 +0900

    feat : 색상 팔레트, 예제 문자열 상수 생성

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/MyConst.kt
@@ -0,0 +1,18 @@
+package woowacourse.kanban.board
+
+// color palette
+const val BOARD_BACKGROUND_COLOR = 0xffFFFFFF
+const val CARD_BACKGROUND_COLOR = 0xffE5E7EB
+const val TITLE_COLOR = 0xff101828
+const val DESCRIPTION_COLOR = 0xff4A5565
+const val TAG_BACKGROUND_COLOR = 0xffF3F4F6
+const val TAG_TEXT_COLOR = 0xff364153
+const val WRITER_TEXT_COLOR = 0xff364153
+
+// example text
+const val EXAMPLE_TITLE = "LazyColumn 컴포넌트 구현"
+const val EXAMPLE_CONTENT = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다."
+const val EXAMPLE_WRITER = "다이노"
+const val MAX_TITLE = "너무너무 긴 제목은 한 줄까지만 노출합니다."
+const val MAX_CONTENT = "너무너무너무 긴 설명은 두 줄까지만 노출하고 말줄임표로 처리합니다 두 줄까지만 노출합니다."
+const val MAX_WRITER = "너무너무너무 긴 담당자도 한 줄까지만 노출합니다."
