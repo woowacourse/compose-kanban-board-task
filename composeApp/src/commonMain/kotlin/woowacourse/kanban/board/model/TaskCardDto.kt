f1e66a27fca1f489e2f118bd5e51a1c510ef91d2
todays_sun_day <178suna@gmail.com>
2026-03-06 17:54:30 +0900

    feat: TaskCardDto 내에서 태그 개수 및 글자수 제한

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/TaskCardDto.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/TaskCardDto.kt
@@ -7,14 +7,19 @@ data class TaskCardDto(
     val contents: String,
     val tags: List<String>,
     val author: String,
-)
+) {
+    init {
+        require(tags.size <= 5) { "[ERROR] 태그 개수는 5개 이하여야 합니다." }
+        require(tags.all { it.length <= 5} ) { "[ERROR] 태그 길이는 5자 이하여야 합니다." }
+    }
+}
 
  class TaskCardDtoProvider : PreviewParameterProvider<TaskCardDto> {
     override val values: Sequence<TaskCardDto> = sequenceOf(
         TaskCardDto(
             title = "제목",
             contents = "내용",
-            tags = listOf("태그"),
+            tags = listOf("태그", "태그222"),
             author = "별터"
         )
     )
