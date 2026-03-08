451e49921cb7f960096b88707cdd50518748c6d1
yws <dbdntjs620@gmail.com>
2026-03-08 17:29:02 +0900

    feat : CardData.kt 수정
    - 객체 생성 방식 변경
    - 각 필드 검증 코드 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/CardData.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/CardData.kt
@@ -2,7 +2,33 @@ package woowacourse.kanban.board
 
 data class CardData(
     val title: String,
-    val content: String?,
-    val tags: List<String>,
+    val content: String? = null,
+    val tags: List<String> = listOf(),
     val writer: String
-)
+) {
+    companion object {
+        fun createCard(
+            title: String,
+            content: String? = null,
+            tags: List<String> = listOf<String>(),
+            writer: String
+        ): CardData {
+
+            require(title.isNotBlank()) { "카드의 제목을 입력해주세요." }
+            require(writer.isNotBlank()) { "작성자의 닉네임을 입력해주세요." }
+            require(tags.size < 6) { "태그는 5개까지 입력 가능합니다." }
+            require(checkTagLength(tags)) { "한 태그에는 최대 5자까지 입력 가능합니다." }
+
+            return CardData(title, content, tags, writer)
+        }
+
+        private fun checkTagLength(tags: List<String>): Boolean {
+            for(tag in tags) {
+                if(tag.length > 6) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+}
