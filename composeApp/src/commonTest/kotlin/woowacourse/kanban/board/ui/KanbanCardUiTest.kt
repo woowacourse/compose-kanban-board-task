4c17e606e1a83e6ab97c44fc944bcaf98e973866
parkhyomi <phyomi3@naver.com>
2026-03-09 17:30:47 +0900

    test: ui 전체 적용시 테스트 코드 구현

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/KanbanCardUiTest.kt
@@ -0,0 +1,42 @@
+package woowacourse.kanban.board.ui
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.runComposeUiTest
+import woowacourse.kanban.board.component.KanbanCard
+import woowacourse.kanban.board.component.card.domain.Title
+import woowacourse.kanban.board.component.card.preview.CONTENT_EX
+import woowacourse.kanban.board.component.card.preview.NAME_EX
+import woowacourse.kanban.board.component.card.preview.TAG_GROUP_EX
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class)
+class KanbanCardUiTest {
+
+    @Test
+    fun `모든 정보가 있으면 모든 필드가 노출된다`() = runComposeUiTest {
+        //give
+        setContent {
+            KanbanCard(
+                title = Title.DEFAULT_TITLE,
+                content = CONTENT_EX,
+                tags = TAG_GROUP_EX,
+                name = NAME_EX,
+            )
+        }
+
+        //when
+        val title = onNodeWithText(Title.DEFAULT_TITLE)
+        val content = onNodeWithText(CONTENT_EX)
+        val firstTag = onNodeWithText(TAG_GROUP_EX[0])
+        val lastTag = onNodeWithText(TAG_GROUP_EX[1])
+        val profile = onNodeWithText(NAME_EX)
+
+        //than
+        title.assertExists()
+        content.assertExists()
+        firstTag.assertExists()
+        lastTag.assertExists()
+        profile.assertExists()
+    }
+}
