74e966ac15946bdb43ad5382a73b40f27b7fbcae
byunghyunkim0 <bb05qudgus@gamil.com>
2026-03-09 14:13:14 +0900

    test: kanbanCardTags 테스트 코드 작성
    - 빈 태그 리스트가 들어올 경우 태그 영역이 렌더링 되지 않는다.

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCardTags.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/KanbanCardTags.kt
@@ -3,11 +3,14 @@ package woowacourse.kanban.board.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun KanbanCardTags(tags: List<String>) {
     FlowRow(
+        modifier = Modifier.testTag("tags"),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalArrangement = Arrangement.spacedBy(4.dp),
     ) {
---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/KanbanCardTagsTest.kt
@@ -0,0 +1,24 @@
+package woowacourse.kanban.board.component
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.runComposeUiTest
+import org.junit.Test
+
+@OptIn(ExperimentalTestApi::class)
+class KanbanCardTagsTest {
+    @Test
+    fun `빈 태그 리스트 테스트`() = runComposeUiTest {
+        val kanbanCardForm = KanbanCardForm(title = "LazyColumn 컴포넌트 구현", crewName = "다이노")
+        val content = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다."
+
+        setContent {
+            KanbanCard(
+                kanbanCardForm = kanbanCardForm,
+                content = content,
+            )
+        }
+
+        onNodeWithTag("tags").assertDoesNotExist()
+    }
+}
