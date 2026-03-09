8031e665478eca06942abb668914cd676a80c907
parkhyomi <phyomi3@naver.com>
2026-03-09 17:23:30 +0900

    test: 도메인 태그 테스트 코드 구현

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/domain/TagsTest.kt
@@ -0,0 +1,58 @@
+package woowacourse.kanban.board.domain
+
+import woowacourse.kanban.board.component.card.domain.Kanban
+import woowacourse.kanban.board.component.card.domain.Tags
+import woowacourse.kanban.board.component.card.domain.Title
+import woowacourse.kanban.board.component.card.preview.CONTENT_EX
+import woowacourse.kanban.board.component.card.preview.NAME_EX
+import woowacourse.kanban.board.component.card.preview.TAG_EX
+import woowacourse.kanban.board.component.card.preview.TAG_GROUP_MAX_EX
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class TagsTest {
+
+    @Test
+    fun `태그가 공백이라면 태그를 생성할 수 없다`() {
+        //give
+        val blankTag = "   "
+
+        //when
+        val tagBlank = assertFailsWith<IllegalArgumentException> { Tags(blankTag) }
+
+        //than
+        assertEquals(Tags.TAGS_WANNING_MESSAGE, tagBlank.message)
+    }
+
+    @Test
+    fun `태그가 정상 입력이면 태그가 정상 생성된다`() {
+        //give
+        val tag = Tags(TAG_EX)
+
+        //when
+        val tagInput = tag.text
+
+        //than
+        assertEquals(TAG_EX, tagInput)
+    }
+
+    @Test
+    fun `태그는 5개까지만 유지되고 각 태그는 5글자로 제한된다`() {
+        //give
+        val task = Kanban(
+            title = Title.DEFAULT_TITLE,
+            content = CONTENT_EX,
+            tags = TAG_GROUP_MAX_EX,
+            profileName = NAME_EX
+        )
+
+        //when
+        val size = task.tags.size
+        val firstTag = task.tags.first().text
+
+        //than
+        assertEquals(Tags.MAX_TAG_COUNT, size)
+        assertEquals(TAG_GROUP_MAX_EX[0], firstTag)
+    }
+}
