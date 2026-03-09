88441a2c5cce3e90b2c7aad1f716317a0b219345
parkhyomi <phyomi3@naver.com>
2026-03-09 17:30:09 +0900

    test: ui 프로필 테스트 코드 구현

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/ProfileTest.kt
@@ -0,0 +1,64 @@
+package woowacourse.kanban.board.ui
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.runComposeUiTest
+import woowacourse.kanban.board.component.KanbanCard
+import woowacourse.kanban.board.component.card.domain.ProfileName
+import woowacourse.kanban.board.component.card.domain.Title
+import woowacourse.kanban.board.component.card.preview.CONTENT_EX
+import woowacourse.kanban.board.component.card.preview.NAME_EX
+import woowacourse.kanban.board.component.card.preview.TAG_GROUP_EX
+import woowacourse.kanban.board.component.card.ui.Profile
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+@OptIn(ExperimentalTestApi::class)
+class ProfileTest {
+
+    @Test
+    fun `닉네임이 있으면 프로필이 노출된다`() = runComposeUiTest {
+        //give
+        setContent {
+            KanbanCard(
+                title = Title.DEFAULT_TITLE,
+                content = CONTENT_EX,
+                tags = TAG_GROUP_EX,
+                name = NAME_EX,
+            )
+        }
+
+        //when
+        val profile = onNodeWithContentDescription("프로필 이미지")
+
+        //than
+        profile.assertExists()
+    }
+
+    @Test
+    fun `닉네임이 공백이면 카드를 생성할 수 없다`() {
+        //give
+        val blankName = "   "
+
+        //when
+        val nameBlank = assertFailsWith<IllegalArgumentException> {
+            runComposeUiTest {
+                setContent {
+                    KanbanCard(
+                        title = Title.DEFAULT_TITLE,
+                        content = CONTENT_EX,
+                        tags = TAG_GROUP_EX,
+                        name = blankName,
+                    )
+                }
+            }
+        }
+
+        //than
+        assertEquals(
+            ProfileName.PROFILE_WANNING_MESSAGE,
+            nameBlank.message,
+        )
+    }
+}
